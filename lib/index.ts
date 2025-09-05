eab41adfde4b01b463f9be153ae448ef9022ccfd
miguel-merlin <mmerlin@stevens.edu>
2025-09-05 10:24:09 -0400

    feat: added website deployment via s3 and cloudfont

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,137 @@
-// import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfont from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+import * as cdk from "aws-cdk-lib";
+import { CanonicalUserPrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { ARecord, HostedZone } from "aws-cdk-lib/aws-route53";
+
+export interface DomainConfig {
+  /** The root domain name (e.g., example.com).
+   * There must be an associated hosted zone in Route 53 for this domain.
+   */
+  domainName: string;
+  /** The subdomain name */
+  subdomainName: string;
+  /** The ARN of the SSL certificate to use for the domain. */
+  certificateArn: string;
+}
 
 export interface WebsiteConstructProps {
-  // Define construct properties here
+  /** The name of the S3 bucket that will host the website content. */
+  bucketName: string;
+
+  /** The path to the index document that will be served as the default page. */
+  indexFile: string;
+
+  /** The path to the error document that will be served when an error occurs. */
+  errorFile: string;
+
+  /** Optional configuration for custom domain setup. */
+  domainConfig?: DomainConfig;
+
+  /** Optional path to a custom 404 page. If not specified, the error file will be used. */
+  notFoundResponsePagePath?: string;
 }
 
 export class WebsiteConstruct extends Construct {
-
-  constructor(scope: Construct, id: string, props: WebsiteConstructProps = {}) {
+  constructor(scope: Construct, id: string, props: WebsiteConstructProps) {
     super(scope, id);
+    const webBucket = new s3.Bucket(this, props.bucketName, {
+      websiteIndexDocument: props.indexFile,
+      websiteErrorDocument: props.errorFile,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
+    const oai = new cloudfont.OriginAccessIdentity(
+      this,
+      `${props.bucketName}-OAI`,
+    );
+    webBucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [webBucket.arnForObjects("*")],
+        principals: [
+          new CanonicalUserPrincipal(
+            oai.cloudFrontOriginAccessIdentityS3CanonicalUserId,
+          ),
+        ],
+      }),
+    );
 
-    // Define construct contents here
+    const distribution = new cloudfont.Distribution(
+      this,
+      `${props.bucketName}-distribution`,
+      {
+        defaultBehavior: {
+          origin: new origins.S3StaticWebsiteOrigin(webBucket),
+          viewerProtocolPolicy:
+            cloudfont.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        errorResponses: [
+          {
+            httpStatus: 404,
+            responseHttpStatus: 404,
+            responsePagePath: props.notFoundResponsePagePath || `/404.html`,
+            ttl: cdk.Duration.minutes(30),
+          },
+        ],
+        priceClass: cloudfont.PriceClass.PRICE_CLASS_100,
+        ...(props.domainConfig
+          ? {
+              domainNames: [this._getFullDomainName(props.domainConfig)],
+              certificate: this._getCertificate(
+                props.domainConfig.certificateArn,
+              ),
+            }
+          : {}),
+      },
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WebsiteConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if (props.domainConfig) {
+      const hostedZone = HostedZone.fromLookup(this, "HostedZone", {
+        domainName: props.domainConfig.domainName,
+      });
+      const domainARecord = new ARecord(this, "DomainARecord", {
+        zone: hostedZone,
+        recordName: this._getFullDomainName(props.domainConfig),
+        target: cdk.aws_route53.RecordTarget.fromAlias(
+          new cdk.aws_route53_targets.CloudFrontTarget(distribution),
+        ),
+      });
+      domainARecord.node.addDependency(distribution);
+    }
+
+    new cdk.CfnOutput(this, "cloudfront-website-url", {
+      value: distribution.distributionDomainName,
+      description: "CloudFront Distribution Domain Name",
+    });
+
+    new cdk.CfnOutput(this, "s3-website-url", {
+      value: webBucket.bucketWebsiteUrl,
+      description: "S3 Bucket Website URL",
+    });
+
+    if (props.domainConfig) {
+      new cdk.CfnOutput(this, "website-url", {
+        value: distribution.domainName,
+        description: "Website URL",
+      });
+    }
+  }
+
+  private _getFullDomainName(domainConfig: DomainConfig): string {
+    return domainConfig.subdomainName
+      ? `${domainConfig.subdomainName}.${domainConfig.domainName}`
+      : domainConfig.domainName;
+  }
+
+  private _getCertificate(arn: string): certificatemanager.ICertificate {
+    return certificatemanager.Certificate.fromCertificateArn(
+      this,
+      `website-cert`,
+      arn,
+    );
   }
 }
--- test/website-construct.test.ts
+++ test/website-construct.test.ts
@@ -1,18 +1,321 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as WebsiteConstruct from '../lib/index';
+import { Template, Match } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import { WebsiteConstruct, WebsiteConstructProps, DomainConfig } from "../lib";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/index.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   const stack = new cdk.Stack(app, "TestStack");
-//   // WHEN
-//   new WebsiteConstruct.WebsiteConstruct(stack, 'MyTestConstruct');
-//   // THEN
-//   const template = Template.fromStack(stack);
+describe("WebsiteConstruct", () => {
+  let app: cdk.App;
+  let stack: cdk.Stack;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new cdk.Stack(app, "TestStack", {
+      env: { account: "123456789012", region: "us-east-1" },
+    });
+    stack.node.setContext(
+      "hosted-zone:account=123456789012:domainName=example.com:region=us-east-1",
+      {
+        Id: "/hostedzone/Z123456789012",
+        Name: "example.com.",
+      },
+    );
+  });
+
+  describe("Basic functionality", () => {
+    test("creates S3 bucket with basic configuration", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::S3::Bucket", {
+        WebsiteConfiguration: {
+          IndexDocument: "index.html",
+          ErrorDocument: "error.html",
+        },
+        BucketEncryption: {
+          ServerSideEncryptionConfiguration: [
+            {
+              ServerSideEncryptionByDefault: {
+                SSEAlgorithm: "AES256",
+              },
+            },
+          ],
+        },
+      });
+    });
+
+    test("creates CloudFront Origin Access Identity", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties(
+        "AWS::CloudFront::CloudFrontOriginAccessIdentity",
+        {
+          CloudFrontOriginAccessIdentityConfig: {
+            Comment: Match.anyValue(),
+          },
+        },
+      );
+    });
+
+    test("creates CloudFront distribution with correct configuration", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::CloudFront::Distribution", {
+        DistributionConfig: {
+          DefaultCacheBehavior: {
+            ViewerProtocolPolicy: "redirect-to-https",
+          },
+          CustomErrorResponses: [
+            {
+              ErrorCode: 404,
+              ResponseCode: 404,
+              ResponsePagePath: "/404.html",
+              ErrorCachingMinTTL: 1800,
+            },
+          ],
+          PriceClass: "PriceClass_100",
+          Enabled: true,
+        },
+      });
+    });
+  });
+
+  describe("Custom error page configuration", () => {
+    test("uses custom not found response page path", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+        notFoundResponsePagePath: "/custom-404.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::CloudFront::Distribution", {
+        DistributionConfig: {
+          CustomErrorResponses: [
+            {
+              ErrorCode: 404,
+              ResponseCode: 404,
+              ResponsePagePath: "/custom-404.html",
+              ErrorCachingMinTTL: 1800,
+            },
+          ],
+        },
+      });
+    });
+
+    test("uses default 404.html when notFoundResponsePagePath is not provided", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::CloudFront::Distribution", {
+        DistributionConfig: {
+          CustomErrorResponses: [
+            {
+              ErrorCode: 404,
+              ResponseCode: 404,
+              ResponsePagePath: "/404.html",
+              ErrorCachingMinTTL: 1800,
+            },
+          ],
+        },
+      });
+    });
+  });
+
+  describe("Domain configuration", () => {
+    const domainConfig: DomainConfig = {
+      domainName: "example.com",
+      subdomainName: "www",
+      certificateArn:
+        "arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012",
+    };
+
+    test("configures CloudFront distribution with custom domain", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+        domainConfig,
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::CloudFront::Distribution", {
+        DistributionConfig: {
+          Aliases: ["www.example.com"],
+          ViewerCertificate: {
+            AcmCertificateArn: domainConfig.certificateArn,
+            SslSupportMethod: "sni-only",
+          },
+        },
+      });
+    });
+
+    test("creates Route53 A record when domain config is provided", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+        domainConfig,
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::Route53::RecordSet", {
+        Type: "A",
+        Name: "www.example.com.",
+        AliasTarget: {
+          DNSName: Match.anyValue(),
+          HostedZoneId: Match.anyValue(),
+        },
+      });
+    });
+
+    test("handles domain without subdomain", () => {
+      const domainConfigWithoutSub: DomainConfig = {
+        domainName: "example.com",
+        subdomainName: "",
+        certificateArn:
+          "arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012",
+      };
+
+      const props: WebsiteConstructProps = {
+        bucketName: "test-website-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+        domainConfig: domainConfigWithoutSub,
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+
+      template.hasResourceProperties("AWS::CloudFront::Distribution", {
+        DistributionConfig: {
+          Aliases: ["example.com"],
+        },
+      });
+
+      template.hasResourceProperties("AWS::Route53::RecordSet", {
+        Type: "A",
+        Name: "example.com.",
+      });
+    });
+  });
+
+  describe("Private methods", () => {
+    test("_getFullDomainName returns correct domain with subdomain", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      const construct = new WebsiteConstruct(stack, "TestWebsite", props);
+
+      // Access private method through bracket notation for testing
+      const getFullDomainName = (construct as any)._getFullDomainName;
+
+      const domainConfig: DomainConfig = {
+        domainName: "example.com",
+        subdomainName: "blog",
+        certificateArn: "arn:test",
+      };
+
+      const result = getFullDomainName(domainConfig);
+      expect(result).toBe("blog.example.com");
+    });
+
+    test("_getFullDomainName returns root domain when subdomain is empty", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      const construct = new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const getFullDomainName = (construct as any)._getFullDomainName;
+
+      const domainConfig: DomainConfig = {
+        domainName: "example.com",
+        subdomainName: "",
+        certificateArn: "arn:test",
+      };
+
+      const result = getFullDomainName(domainConfig);
+      expect(result).toBe("example.com");
+    });
+  });
+
+  describe("Edge cases", () => {
+    test("handles minimal configuration", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "minimal-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      expect(() => {
+        new WebsiteConstruct(stack, "TestWebsite", props);
+      }).not.toThrow();
+
+      const template = Template.fromStack(stack);
+      template.resourceCountIs("AWS::S3::Bucket", 1);
+      template.resourceCountIs("AWS::CloudFront::Distribution", 1);
+      template.resourceCountIs(
+        "AWS::CloudFront::CloudFrontOriginAccessIdentity",
+        1,
+      );
+    });
+
+    test("does not create Route53 resources when domain config is not provided", () => {
+      const props: WebsiteConstructProps = {
+        bucketName: "test-bucket",
+        indexFile: "index.html",
+        errorFile: "error.html",
+      };
+
+      new WebsiteConstruct(stack, "TestWebsite", props);
+
+      const template = Template.fromStack(stack);
+      template.resourceCountIs("AWS::Route53::RecordSet", 0);
+    });
+  });
 });
